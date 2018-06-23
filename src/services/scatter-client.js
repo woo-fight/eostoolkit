8e785ad0a0de52b223470187a8a89e6cd1d59392
wbb <binbin.wan@upai.com>
2018-06-24 01:11:38 +0800

    删除无效代码

--- src/services/scatter-client.js
+++ src/services/scatter-client.js
@@ -1,15 +1,9 @@
 import Eos from 'eosjs'
 import config from 'config'
-const mainNetwork = {
-  blockchain: 'eos',
-  host: 'mainnet.genereos.io', // ( or null if endorsed chainId )
-  port: 80, // ( or null if defaulting to 80 )
-  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // Or null to fetch automatically ( takes longer )
-}
 
 class ScatterService {
   constructor(scatter) {
-    this.network = '';//mainNetwork;
+    this.network = '';;
     this.scatter = undefined;
     this.eos = undefined;
     document.addEventListener('scatterLoaded', scatterExtension => {
@@ -20,9 +14,10 @@ class ScatterService {
 
   async load(scatter, network) {
     this.scatter = scatter;
-    if (this.identity) {
-      this.scatter.useIdentity(this.scatter.identity.hash);
-    }
+    // console.log(this.identity, this.scatter.identity.hash);
+    // if (this.identity) {
+    // this.scatter.useIdentity(this.scatter.identity.hash);
+    // }
     this.network = network;
     this.eos = this.scatter.eos(this.network, Eos, {
       broadcast: true,
