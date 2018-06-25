2a8cbddf4622a40a7c4cc0479d134c729a7d84b3
wbb <binbin.wan@upai.com>
2018-06-25 15:21:43 +0800

    增加当前游戏信息查询

--- src/lottery/js/lotterydata.js
+++ src/lottery/js/lotterydata.js
@@ -2,8 +2,7 @@ class LotteryData {
   constructor() {
     this.gamerecords = null;
     this.bettingsrecords = null;
-    this.betperson = null;
-    this.period = null;
+    this.curr_game_info = null;
   }
   async load(lottery, account) {
     let response = { data: {}, errmsg: '' };
@@ -11,8 +10,7 @@ class LotteryData {
     // rows.sort(function(a, b) { return b.high_bid - a.high_bid; })
     // res.json({size: rows.length, data: rows.slice(0, 5)});
     this.gamerecords = response.data;;
-    this.betperson = response.data[this.gamerecords.length - 1].current_index;
-    this.period = response.data[this.gamerecords.length - 1].g_id + 1;
+    this.curr_game_info = response.data[this.gamerecords.length - 1];
 
     response = await lottery.getBettingsRecord(account);
     this.bettingsrecords = response.data;
