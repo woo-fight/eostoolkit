ea284fa2d62802df3b34187f2de7f5d2e5dff887
wbb <binbin.wan@upai.com>
2018-06-30 00:45:03 +0800

    解决网络、token 符号问题

--- src/services/config.js
+++ src/services/config.js
@@ -1,22 +1,21 @@
 const config = {
   contract_account: 'test12345123',
-  bbb: 222,
   customNetwork: {
     blockchain: 'eos',
-    host: 'jungle.cryptolions.io',  // ( or null if endorsed chainId )
-    port: 18888,         // ( or null if defaulting to 80 )
+    host: 'jungle.eosio.cr',  // ( or null if endorsed chainId )
+    port: 443,         // ( or null if defaulting to 80 )
     chainId:
       '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',  // Or null to fetch automatically ( takes longer )
-    protocol: 'http'
+    protocol: 'https'
   },
   environment: {
     // production: false,
     // apiUrl: '//jungle.cryptolions.io:18888',
     // eosHost: 'jungle.cryptolions.io',
     // eosPort: 18888,
-    eosProtocol: 'http',
+    eosProtocol: 'https',
   },
-  token_symnol: 'SYS'
+  token_symnol: 'EOS'
 }
 
 export default config;
